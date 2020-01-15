f41e36c56ed89c0000ae789522f2bff908f6c2fc
ricardosikic <ricardo.sikic@gmail.com>
2020-01-15 20:08:19 +0000

    explicacion de closure.

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ let val = function_a(3, 3);
 console.log('valor :', val); //18
 
 
+// Los closures: son funciones que no poseen variables propias sino que heredan
+// variables o argumentos de la funcion padre que la contiene.
+
+
+/* function padre(y) {
+  function closure(h) {
+    return h;
+  }
+  closure(y);
+}
+ */
+
+
 triangulo = (a, b) => {
   creaTriangulo = (c, d) => {
     return (c * d) / 2;
